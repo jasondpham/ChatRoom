08d91732bcdcb1b7b52957fb69d3d4e069d37e59
Jason Pham <70985186+phamja1@users.noreply.github.com>
2021-12-24 22:50:53 -0600

    Add temp login feature, change User model

--- model/Users.go
+++ model/Users.go
@@ -4,5 +4,5 @@ type User struct {
 	Id       uint   `json:"id"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
-	Password string `json:"password"`
+	Password []byte `json:"password"`
 }
--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,7 +11,7 @@ import (
 	"example.com/model"
 	"github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
-  "golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var db *sql.DB
@@ -44,18 +45,38 @@ func main() {
 }
 
 func getUser(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello world")
+  user := make(map[string]string)
+
+  if err := json.NewDecoder(c.Request().Body).Decode(&user); err != nil {
+    return err
+  }
+
+  row := db.QueryRow("SELECT * from users where email = ?", user["email"])
+
+  entry := new(model.User)
+  if err := row.Scan(&entry.Id, &entry.Name, &entry.Email, &entry.Password); err != nil {
+    return err
+  }
+
+  if err := bcrypt.CompareHashAndPassword(entry.Password, []byte(user["password"])); err != nil {
+    return c.JSON(http.StatusUnauthorized, echo.Map {
+      "message": "incorrect password",
+    })
+  }
+  return c.JSON(http.StatusOK, echo.Map {
+    "name": entry.Name,
+  })
 }
 
 func register(c echo.Context) error {
-	u := new(model.User)
-	if err := c.Bind(u); err != nil {
+	u := make(map[string]string) 
+	if err := json.NewDecoder(c.Request().Body).Decode(&u); err != nil {
 		return err
 	}
   
-  password, _ := bcrypt.GenerateFromPassword([]byte(u.Password), 14)
+  password, _ := bcrypt.GenerateFromPassword([]byte(u["password"]), 14)
 
-	_, err := db.Exec("INSERT INTO users (name, email, password) values (?, ?, ?)", u.Name, u.Email, password)
+	_, err := db.Exec("INSERT INTO users (name, email, password) values (?, ?, ?)", u["name"], u["email"], password)
 
 	if err != nil {
 		return err
